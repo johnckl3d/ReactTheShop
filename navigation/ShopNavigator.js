0b482c6ca0f236702a4fd6437595b7e1e2dd1757
Cheangkhailung <johnckl3d@gmail.com>
2020-02-13 23:43:54 +0800

    update

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -3,15 +3,18 @@ import {createStackNavigator } from 'react-navigation-stack';
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import Colors from '../constants/Colors';
 import { Platform } from 'react-native';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 
 const ProductsNavigator = createStackNavigator({
-    ProductsOverview: ProductsOverviewScreen
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen
+
 }, {
 defaultNavigationOptions: {
     headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+        backgroundColor: Colors.primary
     },
-    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+    headerTintColor: 'white'
 }
 }
 );
