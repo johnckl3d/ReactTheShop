463be5d78ece9ad49099024d8fad19852c5bb4b8
Cheangkhailung <johnckl3d@gmail.com>
2020-02-15 23:24:41 +0800

    before change to class

---
+++ components/UI/HeaderButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { HeaderButton } from "react-navigation-header-buttons";
+//import { Ionicons } from "@expo/vector-icons";
+import Icon from 'react-native-ionicons';
+import Colors from "../../constants/Colors";
+
+const CustomHeaderButton = props => {
+  return (
+    <HeaderButton
+      {...props}
+      IconComponent={Icon}
+      iconSize={23}
+      color={"white"}
+    />
+  );
+};
+
+export default CustomHeaderButton;
--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -73,12 +73,12 @@ const styles = StyleSheet.create({
     height: "100%"
   },
   title: {
-    fontFamily: 'open-sans-bold',
+    //fontFamily: 'open-sans-bold',
     fontSize: 18,
     marginVertical: 2
   },
   price: {
-    fontFamily: 'open-sans',
+    //fontFamily: 'open-sans',
     fontSize: 14,
     color: "#888"
   },
