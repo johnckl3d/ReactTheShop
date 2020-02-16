12b75843d238f4778ba9d64f36d3a5c0ee3ec9d6
Cheangkhailung <johnckl3d@gmail.com>
2020-02-16 17:46:48 +0800

    added cart

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,28 +1,31 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator } from 'react-navigation-stack';
-import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
-import Colors from '../constants/Colors';
-import { Platform } from 'react-native';
-import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import Colors from "../constants/Colors";
+import { Platform } from "react-native";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+import CartScreen from "../screens/shop/CartScreen";
 
-const ProductsNavigator = createStackNavigator({
+const ProductsNavigator = createStackNavigator(
+  {
     ProductsOverview: ProductsOverviewScreen,
-    ProductDetail: ProductDetailScreen
-
-}, {
-defaultNavigationOptions: {
-    headerStyle: {
+    ProductDetail: ProductDetailScreen,
+    Cart: CartScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
         backgroundColor: Colors.primary
-    },
-    headerTitleStyle: {
-        fontFamily: 'open-sans-bold'
-    },
-    headerBackTitleStyle: {
-        fontFamily: 'open-sans'
-    },
-    headerTintColor: 'white'
-}
-}
+      },
+      headerTitleStyle: {
+        fontFamily: "open-sans-bold"
+      },
+      headerBackTitleStyle: {
+        fontFamily: "open-sans"
+      },
+      headerTintColor: "white"
+    }
+  }
 );
 
 export default createAppContainer(ProductsNavigator);
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,9 +2,9 @@ import React from "react";
 import { FlatList } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import ProductItem from "../../components/shop/ProductItem";
-import * as cartActions from '../../store/actions/cart';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import HeaderButton from '../../components/UI/HeaderButton';
+import * as cartActions from "../../store/actions/cart";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../../components/UI/HeaderButton";
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
@@ -33,12 +33,17 @@ const ProductsOverviewScreen = props => {
   );
 };
 
-ProductsOverviewScreen.navigationOptions = {
-  headerTitle: 'All Products',
-  headerRight: <HeaderButtons HeaderButtonComponent={HeaderButton}>
-    <Item title='Cart' iconName={'ios-cart'} onPress={() => {}}/>
-
-  </HeaderButtons>
-}
+ProductsOverviewScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "All Products",
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title="Cart" iconName={"ios-cart"} onPress={() => {
+          navData.navigation.navigate('Cart');
+        }} />
+      </HeaderButtons>
+    )
+  };
+};
 
 export default ProductsOverviewScreen;
